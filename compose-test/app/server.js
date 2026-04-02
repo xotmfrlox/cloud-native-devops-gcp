d56191fa1b8aeb255d7a2aeb8b8a8e10938e8f45
Seulgi Choi <sggg.choi@gmail.com>
2026-04-02 14:44:41 +0200

    Add docker-compose and fix DB connection

---
+++ compose-test/app/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { Client } = require('pg');
+
+const app = express();
+
+const client = new Client({
+  host: 'postgres',
+  user: 'postgres',
+  password: '1234',
+  database: 'appdb',
+});
+
+let dbReady = false;
+
+const connectDB = async () => {
+  while (true) {
+    try {
+      const newClient = new Client({
+        host: 'postgres',
+        user: 'postgres',
+        password: '1234',
+        database: 'appdb',
+      });
+
+      await newClient.connect();
+      console.log("DB connected");
+
+      global.client = newClient;
+      dbReady = true;
+      break;
+
+    } catch (err) {
+      console.log("DB not ready, retrying...");
+      await new Promise(res => setTimeout(res, 2000));
+    }
+  }
+};
+
+connectDB();
+
+app.get('/', async (req, res) => {
+  if (!dbReady) {
+    return res.send("DB not ready yet");
+  }
+
+  const result = await global.client.query('SELECT NOW()');
+  res.send(`DB Time: ${result.rows[0].now}`);
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
